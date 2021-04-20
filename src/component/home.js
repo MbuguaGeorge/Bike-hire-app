9eb11b8372dd135c7c7bdbd79037a618dc20350f
unknown <mbuguag026@gmail.com>
2021-04-20 23:14:54 +0300

    toggle signup form

--- src/component/home.js
+++ src/component/home.js
@@ -1,15 +1,27 @@
 import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import Signup from './signup';
 
 function Home(){
+
+    const [register, setRegister] = useState(false);
+
+    function clickHandler(){
+        setRegister(prevRegister => !prevRegister)
+    }
+
     return(
         <div>
             <section>
+                <div className="sign" style={{display: register ? "flex" : "none"}} >
+                    <Signup />
+                </div>
                 <div className="home__page">
                     <div className="cover__page">
                         <h2>Bike rental services</h2>
                         <p>We provide a reliable platform for hiring and renting out bikes.</p>
                         <div className="forms">
-                            <Link to="/signup"><button>Sign up</button></Link>
+                            <button onClick={clickHandler} >Sign up</button>
                             <Link to="/login"><button>Log in</button></Link>
                         </div>
                     </div>
