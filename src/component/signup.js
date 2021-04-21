e16d4015099587be6972fe7c154dc3bb628bacc6
unknown <mbuguag026@gmail.com>
2021-04-21 14:46:48 +0300

    form validation

--- src/component/signup.js
+++ src/component/signup.js
@@ -1,24 +1,78 @@
 import React,{Component} from 'react';
 
+const initialState = {
+    nameError: '',
+    emailError: '',
+    passwordError: '',
+    password2Error: ''
+}
+
 class Signup extends Component{
 
     state = {
-        details: {username: '', email: '', password: '', password2: ''}
+        details: {
+            username: '',
+            email: '',
+            password: '',
+            password2: ''
+        },
+        initialState
     }
+
+    validate = () => {
+        let emailError = '';
+        let nameError = '';
+        let passwordError = '';
+        let password2Error = '';
+
+        if (!this.state.details.password2){
+            password2Error = 'cannot be blank';
+        }
+
+        if (this.state.details.password !== this.state.details.password2){
+            passwordError = 'passwords do not match'
+        }
+
+        if (!this.state.details.password){
+            passwordError = 'cannot be blank';
+        }
+
+        if (!this.state.details.username){
+            nameError = 'cannot be blank';
+        }
+
+        if (!this.state.details.email.includes('@')) {
+            emailError = 'invalid email address enter valid';
+        }
+
+        if (!this.state.details.email){
+            emailError = 'cannot be blank';
+        }
+
+        if (emailError || nameError || passwordError || password2Error ) {
+            this.setState({emailError, nameError, passwordError, password2Error});
+            return false;
+        }
+        return true;
+    };
 
     submit = event => {
         event.preventDefault();
-        console.log(this.state.details);
-        fetch('http://127.0.0.1:8000/register', {
-            method: 'POST',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify(this.state.details)
-        }).then(
-            data => {
-                console.log(data);
-            }
-        ).catch( error =>console.error(error) )
-    }
+        const isValid = this.validate();
+        if (isValid) {
+            console.log(this.state.details);
+            this.setState(initialState);
+            fetch('http://127.0.0.1:8000/register', {
+                method: 'POST',
+                headers: {'Content-Type' : 'application/json'},
+                body: JSON.stringify(this.state.details)
+            }).then(
+                data => {
+                    console.log(data);
+                }
+            ).catch( error =>console.error(error) )
+        } 
+    };
 
     inputChange = event => {
         const detail = this.state.details;
@@ -38,6 +92,9 @@ class Signup extends Component{
                             value={this.state.details.username}
                             onChange={this.inputChange}
                         />
+                        <div className="error">
+                            <h5>{this.state.nameError}</h5>
+                        </div>
                         <input 
                             type="text"
                             name="email"
@@ -45,6 +102,9 @@ class Signup extends Component{
                             value={this.state.details.email}
                             onChange={this.inputChange}
                         />
+                        <div className="error">
+                            <h5>{this.state.emailError}</h5>
+                        </div>
                         <input 
                             type="password"
                             name="password"
@@ -52,6 +112,9 @@ class Signup extends Component{
                             value={this.state.details.password}
                             onChange={this.inputChange} 
                         />
+                        <div className="error">
+                            <h5>{this.state.passwordError}</h5>
+                        </div>
                         <input 
                             type="password"
                             name="password2"
@@ -59,6 +122,9 @@ class Signup extends Component{
                             value={this.state.details.password2}
                             onChange={this.inputChange}
                         />
+                        <div className="error">
+                            <h5>{this.state.passwordError}</h5>
+                        </div>
                         <button onClick={this.submit}>Submit</button>
                     </form>
                 </div>
